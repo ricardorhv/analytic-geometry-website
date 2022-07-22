b46907557a9f16e5d7fba43c46bfc3e511870d45
Ricardo Henrique Vinciguerra <ricardorhv.dev@gmail.com>
2022-07-21 23:00:13 -0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/analytic-geometry-website/",
   plugins: [react()],
-  base: "https://ricardorhv.github.io/analytic-geometry-website/",
-  build: {
-    outDir: 'build'
-  }
 })
